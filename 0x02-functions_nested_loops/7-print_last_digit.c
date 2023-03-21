b9ab8607738210bbfab9110503ec6ddf7db373fc
hazem <hazem@gmail.com>
2023-03-21 16:59:23 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_last_digit-function print last digit
+*@n: is argument checked
+*
+*Return: function return last digit of number after check abs
+*/
+int print_last_digit(long int n)
+{
+
+if (n < 0)
+{
+
+n = n * -1;
+}
+
+n = n % 10;
+
+_putchar(n + '0');
+
+return (n);
+}
