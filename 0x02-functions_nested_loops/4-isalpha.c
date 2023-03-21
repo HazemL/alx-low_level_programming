06e6b5d04aabb184b2d37e5222f632f70bbb3d10
hazem <hazem@gmail.com>
2023-03-21 16:34:04 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/*
+ * Returns 1 if c is a letter, lowercase or uppercase
+ *
+ * Returns 0 otherwise
+ */
+int _isalpha(int c)
+{
+if (isalpha(c) == 0)
+{
+return (0);	
+}
+else
+{
+return (1);
+}
+}
