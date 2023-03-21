d241a48e0fe31290b77f900029952f041a1b8260
hazem <hazem@gmail.com>
2023-03-21 16:55:32 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_abs-function return absolute value
+*@n: paramter for enter
+*
+*Return: return number after applay abs
+*
+*/
+int _abs(int n)
+{
+
+if (n < 0)
+{
+n = n * -1;
+}
+return (n);
+}
+
