9459612d928a40393f6cb3b8a70f6cc35219efd1
hazem <hazem@gmail.com>
2023-03-24 18:10:57 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,33 +1,47 @@
-/*include unistd library*/
-#include <unistd.h>
+#include "main.h"
 
-/*making this so the compiler could know what type of function putchar is.*/
-void        ft_putchar(char c)
-            {
-                    write(1, &c, 1);
-            }
+/**
+ * print_number - Function that prints an integer.
+ * @n: int type number
+ * Description: Can only use _putchar to print.
+ */
+void print_number(int n)
+{
+	long m; /* power of 10 */
+	int c; /* boolean check */
+	long num; /* convert int to long */
 
-/*main just called ft_putstr which is prototyped as having a char return value. Sidenote: a function prototype tells the compiler what sort of function ft_putstr is*/
+	num = n;
+	/* negatives */
+	if (num < 0)
+	{
+		num *= -1;
+		_putchar('-');
+	}
 
-/*Here we will define the function.*/
+	/* count up */
+	m = 1;
+	c = 1;
+	while (c)
+	{
+		if (num / (m * 10) > 0)
+			m *= 10;
+		else
+			c = 0;
+	}
 
-/*iterate through the string. Since I can't use strlen() and since strlen stops at a "\0" character anyway, I'll declare an int variable of i and set its value to 0. Then i'll use a while loop to iterate through the string input and put it's character out the the OS (I think that's right) using putchar and passing in the str[i] as the argument. The loop willincrease by one until it reaches a the last bit character stored in it's memory address, which is "\0"*/
-
-void        ft_putstr(char *str)
-            {
-                    int i;    
-                    i = 0;   
-                    while (str[i] != '\0')
-                    {        
-                            ft_putchar(str[i]);
-                            i++;
-                    }
-             }
-    
-    
-// included for test. Should not be pushed with the answer
-int       main()
-          {
-                  ft_putstr("hello");
-                  return (0);
-          }}
+	/* count down */
+	while (num >= 0)
+	{
+		if (m == 1)
+		{
+			_putchar(num % 10 + '0');
+			num = -1;
+		}
+		else
+		{
+			_putchar((num / m % 10) + '0');
+			m /= 10;
+		}
+	}
+}
