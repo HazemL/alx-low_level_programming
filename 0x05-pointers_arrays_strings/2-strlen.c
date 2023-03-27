25e7117e41891ceb28eb5879dff36598bd3d637f
hazem <hazem@gmail.com>
2023-03-27 16:07:04 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strlen - function count lenth of arr
+*@s: arr of charachter
+*Return: func return length of arr
+*/
+int _strlen(char *s)
+{
+return (strlen(s));
+}
