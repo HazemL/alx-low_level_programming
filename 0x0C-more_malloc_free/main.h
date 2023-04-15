460dbbb3f42538491d3c8af3c1fa47edc6a2a2ac
hazem <hazem@gmail.com>
2023-04-14 18:48:27 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,3 +6,6 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
